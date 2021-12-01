cdd4171bbe24e7e74e6968c3fa274de7b845af3e
Maurits van der Schee <maurits@vdschee.nl>
2021-12-01 08:06:51 +0100

    update

---
+++ day01/part1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+namespace part1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int result = 0;
+            using (StreamReader reader = new StreamReader("input"))
+            {
+                string line;
+                int previousNumber=-1,currentNumber;
+                while ((line = reader.ReadLine()) != null) {
+                    currentNumber = int.Parse(line);
+                    if (previousNumber!=-1 && currentNumber>previousNumber) {
+                        result += 1;
+                    }
+                    previousNumber = currentNumber;
+                }
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
