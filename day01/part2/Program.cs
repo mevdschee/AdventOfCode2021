c417b9295812c4f7379e11a22951fb3413627cf4
Maurits van der Schee <maurits@vdschee.nl>
2021-12-01 08:06:33 +0100

    update

---
+++ day01/part2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace part2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int result = 0;
+            using (StreamReader reader = new StreamReader("input"))
+            {
+                string line;
+                int[] numbers = new int[3]{ -1, -1, -1};
+                int i = 0;
+                int previousSum = -1, currentSum;
+                while ((line = reader.ReadLine()) != null) {
+                    int j = (i+1)%numbers.Length; 
+                    numbers[j] = int.Parse(line);
+                    int currentSum = numbers.Sum();
+                    if (numbers[i]!=-1 && numbers[j]>numbers[i]) {
+                        result += 1;
+                    }
+                    i = j;
+                }
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
