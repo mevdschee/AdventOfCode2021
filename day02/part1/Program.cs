8e7f9f9e3aaccfe9df08d39f0a0508f9a441b631
Maurits van der Schee <maurits@vdschee.nl>
2021-12-02 06:37:56 +0100

    update

---
+++ day02/part1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace part1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 0, y = 0;
+            using (StreamReader reader = new StreamReader("input"))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null) {
+                    var parts = line.Split(' ');
+                    var distance = int.Parse(parts[1]);
+                    switch(parts[0]){
+                        case "forward":
+                            x += distance;
+                            break;
+                        case "down":
+                            y -= distance;
+                            break;
+                        case "up":
+                            y += distance;
+                            break;
+                    } 
+                }
+            }
+            Console.WriteLine(x*(-1*y));
+        }
+    }
+}
---
+++ day02/part2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+
+namespace part1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int position = 0, depth = 0, aim = 0;
+            using (StreamReader reader = new StreamReader("input"))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null) {
+                    var parts = line.Split(' ');
+                    var distance = int.Parse(parts[1]);
+                    switch(parts[0]){
+                        case "forward":
+                            position += distance;
+                            depth += aim*distance;
+                            break;
+                        case "down":
+                            aim += distance;
+                            break;
+                        case "up":
+                            aim -= distance;
+                            break;
+                    } 
+                }
+            }
+            Console.WriteLine(position*depth);
+        }
+    }
+}
