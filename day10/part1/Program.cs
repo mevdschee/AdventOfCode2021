be0d8e12b3f94e55cefe52b34952ca671439c2a2
Maurits van der Schee <maurits@vdschee.nl>
2021-12-10 09:14:33 +0100

    update

---
+++ day10/part1/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lines = new List<string>();
+            using (StreamReader reader = new StreamReader("input"))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    if (line.Length > 0)
+                    {
+                        lines.Add(line);
+                    }
+                }
+            }
+            var points = new Dictionary<char, int>
+            {
+                {')', 3},
+                {']', 57},
+                {'}', 1197},
+                {'>', 25137}
+            };
+            var result = 0;
+            foreach (var line in lines)
+            {
+                var stack = new Stack<char>();
+                foreach (var c in line)
+                {
+                    var chars = "([{<)]}>";
+                    var p = chars.IndexOf(c);
+                    if (p < 4)
+                    {
+                        stack.Push(chars[p + 4]);
+                    }
+                    else
+                    {
+                        var expected = stack.Pop();
+                        if (c != expected)
+                        {
+                            result += points[c];
+                        }
+                    }
+                }
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ day10/part2/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lines = new List<string>();
+            using (StreamReader reader = new StreamReader("input"))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    if (line.Length > 0)
+                    {
+                        lines.Add(line);
+                    }
+                }
+            }
+            var points = new Dictionary<char, int>
+            {
+                {')', 1},
+                {']', 2},
+                {'}', 3},
+                {'>', 4}
+            };
+            var scores = new List<long>();
+            foreach (var line in lines)
+            {
+                var stack = new Stack<char>();
+                var corrupted = false;
+                foreach (var c in line)
+                {
+                    var chars = "([{<)]}>";
+                    var p = chars.IndexOf(c);
+                    if (p < 4)
+                    {
+                        stack.Push(chars[p + 4]);
+                    }
+                    else
+                    {
+                        var expected = stack.Pop();
+                        if (c != expected)
+                        {
+                            corrupted = true;
+                            break;
+                        }
+                    }
+                }
+                if (!corrupted)
+                {
+                    long score = 0;
+                    foreach (var c in stack)
+                    {
+                        score = score * 5 + points[c];
+                    }
+                    scores.Add(score);
+                }
+            }
+            scores.Sort();
+            scores.Reverse();
+            Console.WriteLine(scores[scores.Count/2]);
+        }
+    }
+}
