80fa085e393a60faf475dbb32b5f298170b40129
Maurits van der Schee <maurits@vdschee.nl>
2021-12-16 07:49:20 +0100

    update

--- day15/part2/Program.cs
+++ day15/part2/Program.cs
@@ -9,7 +9,7 @@ namespace Program
         static void Main(string[] args)
         {
             var lines = new List<string>();
-            using (StreamReader reader = new StreamReader("input.test"))
+            using (StreamReader reader = new StreamReader("input"))
             {
                 string line;
                 while ((line = reader.ReadLine()) != null)
@@ -23,23 +23,27 @@ namespace Program
             for (var y = 0; y < lines.Count; y++)
             {
                 var strLength = lines[y].Length;
-                for (var i = 0; i < 5; i++)
+                for (var i = 1; i < 5; i++)
                 {
                     for (var x = 0; x < strLength; x++)
                     {
-                        lines[y] += ((int.Parse(new string(lines[y][x], 1)) + i) % 10).ToString();
+                        var n = int.Parse(new string(lines[y][x], 1)) + i;
+                        n = n > 9 ? (n - 1) % 9 + 1 : n;
+                        lines[y] += n.ToString();
                     }
                 }
             }
             var lineCount = lines.Count;
-            for (var i = 0; i < 5; i++)
+            for (var i = 1; i < 5; i++)
             {
                 for (var y = 0; y < lineCount; y++)
                 {
                     var line = "";
                     for (var x = 0; x < lines[y].Length; x++)
                     {
-                        line += ((int.Parse(new string(lines[y][x], 1)) + i) % 10).ToString();
+                        var n = int.Parse(new string(lines[y][x], 1)) + i;
+                        n = n > 9 ? (n - 1) % 9 + 1 : n;
+                        line += n.ToString();
                     }
                     lines.Add(line);
                 }
