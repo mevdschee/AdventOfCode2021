b1dbcab664b348dc9f3527db813fed0a8ad912ad
Maurits van der Schee <maurits@vdschee.nl>
2021-12-17 14:23:14 +0100

    update

--- day16/part2/Program.cs
+++ day16/part2/Program.cs
@@ -7,12 +7,12 @@ namespace Program
 {
     class Program
     {
-        struct Packet
+        class Packet
         {
-            public int Version;
-            public int Type;
-            public long Literal;
-            public List<Packet> SubPackets;
+            public int Version = 0;
+            public int Type = 0;
+            public long Literal = 0;
+            public List<Packet> SubPackets = new List<Packet>();
 
             public int GetVersionSum()
             {
@@ -103,6 +103,84 @@ namespace Program
             {
                 return SubPackets[0].Evaluate() == SubPackets[1].Evaluate() ? 1 : 0;
             }
+
+            public static Packet FromBytes(byte[] bytes)
+            {
+                var packets = new List<Packet>();
+                ParsePackets(bytes, 0, bytes.Length * 8, 1, packets);
+                return packets[0];
+            }
+
+            private static int ParsePackets(byte[] bytes, int bitpos, int bitlen, int packets, List<Packet> subpackets)
+            {
+                var p = bitpos;
+                while (p < bitpos + bitlen)
+                {
+                    var packet = new Packet();
+                    //Console.WriteLine("packet p={0}", p);
+                    packet.Version = ReadInt(bytes, p, 3); p += 3;
+                    packet.Type = ReadInt(bytes, p, 3); p += 3;
+                    //Console.WriteLine("type {0} p={1}", packet.Type, p - 3);
+                    packet.Literal = 0;
+                    packet.SubPackets = new List<Packet>();
+                    if (packet.Type == 4)
+                    {
+                        var nibbles = new List<long>();
+                        do
+                        {
+                            long nibble = ReadInt(bytes, p + 1, 4); p += 5;
+                            //Console.WriteLine("-nibble {0}", nibble);
+                            packet.Literal = nibble | (packet.Literal << 4);
+                            //Console.WriteLine("-literal {0} p={1}", packet.Literal, p - 5);
+                            nibbles.Add(nibble);
+                        }
+                        while (ReadInt(bytes, p - 5, 1) == 1);
+                        //Console.WriteLine("literal {0}", packet.Literal);
+                    }
+                    else
+                    {
+                        var lenlen = (ReadInt(bytes, p, 1) == 1) ? 11 : 15; p += 1;
+                        var length = ReadInt(bytes, p, lenlen); p += lenlen;
+                        if (lenlen == 11)
+                        {
+                            //Console.WriteLine("packet length {0}", length);
+                            p += ParsePackets(bytes, p, bitlen, length, packet.SubPackets);
+                        }
+                        else
+                        {
+                            //Console.WriteLine("bit length {0}", length);
+                            p += ParsePackets(bytes, p, length, -1, packet.SubPackets);
+                        }
+                    }
+                    subpackets.Add(packet);
+                    packets -= 1;
+                    if (packets == 0)
+                    {
+                        break;
+                    }
+                }
+                return p - bitpos;
+            }
+
+            private static int ReadInt(byte[] bytes, int bitpos, int bitlen)
+            {
+                var bits = new Dictionary<int, (int, int)>();
+                for (var i = 0; i < bitlen; i++)
+                {
+                    bits.Add(i, ((bitpos + i) / 8, (bitpos + i) % 8));
+                }
+                var result = 0;
+                foreach (var i in bits.Keys)
+                {
+                    var (by, bi) = bits[i];
+                    var bit = (bytes[by] & (1 << (7 - bi))) > 0;
+                    if (bit)
+                    {
+                        result |= 1 << (bitlen - 1 - i);
+                    }
+                }
+                return result;
+            }
         }
 
         static void Main(string[] args)
@@ -128,86 +206,9 @@ namespace Program
             //    }
             //}
             //Console.WriteLine();
-            var packet = ParsePacket(bytes);
+            var packet = Packet.FromBytes(bytes);
             Console.WriteLine(packet.Evaluate());
         }
 
-        static Packet ParsePacket(byte[] bytes)
-        {
-            var packets = new List<Packet>();
-            ParsePackets(bytes, 0, bytes.Length * 8, 1, packets);
-            return packets[0];
-        }
-
-        static int ParsePackets(byte[] bytes, int bitpos, int bitlen, int packets, List<Packet> subpackets)
-        {
-            var p = bitpos;
-            while (p < bitpos + bitlen)
-            {
-                var packet = new Packet();
-                //Console.WriteLine("packet p={0}", p);
-                packet.Version = Read(bytes, p, 3); p += 3;
-                packet.Type = Read(bytes, p, 3); p += 3;
-                //Console.WriteLine("type {0} p={1}", packet.Type, p - 3);
-                packet.Literal = 0;
-                packet.SubPackets = new List<Packet>();
-                if (packet.Type == 4)
-                {
-                    var nibbles = new List<long>();
-                    do
-                    {
-                        var nibble = Read(bytes, p + 1, 4); p += 5;
-                        //Console.WriteLine("-nibble {0}", nibble);
-                        packet.Literal = nibble | (packet.Literal << 4);
-                        //Console.WriteLine("-literal {0} p={1}", packet.Literal, p - 5);
-                        nibbles.Add(nibble);
-                    }
-                    while (Read(bytes, p - 5, 1) == 1);
-                    //Console.WriteLine("literal {0}", packet.Literal);
-                }
-                else
-                {
-                    var lenlen = (Read(bytes, p, 1) == 1) ? 11 : 15; p += 1;
-                    var length = Read(bytes, p, lenlen); p += lenlen;
-                    if (lenlen == 11)
-                    {
-                        //Console.WriteLine("packet length {0}", length);
-                        p += ParsePackets(bytes, p, bitlen, length, packet.SubPackets);
-                    }
-                    else
-                    {
-                        //Console.WriteLine("bit length {0}", length);
-                        p += ParsePackets(bytes, p, length, -1, packet.SubPackets);
-                    }
-                }
-                subpackets.Add(packet);
-                packets -= 1;
-                if (packets == 0)
-                {
-                    break;
-                }
-            }
-            return p - bitpos;
-        }
-
-        static int Read(byte[] bytes, int bitpos, int bitlen)
-        {
-            var bits = new Dictionary<int, (int, int)>();
-            for (var i = 0; i < bitlen; i++)
-            {
-                bits.Add(i, ((bitpos + i) / 8, (bitpos + i) % 8));
-            }
-            var result = 0;
-            foreach (var i in bits.Keys)
-            {
-                var (by, bi) = bits[i];
-                var bit = (bytes[by] & (1 << (7 - bi))) > 0;
-                if (bit)
-                {
-                    result |= 1 << (bitlen - 1 - i);
-                }
-            }
-            return result;
-        }
     }
 }
